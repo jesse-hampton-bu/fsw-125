3cbf9fe7ae5679d0d200f45a5ed2c16b4b8f4742
jesse-hampton-bu <65149406+jesse-hampton-bu@users.noreply.github.com>
2021-01-15 22:55:22 -0800

    week3-commit

--- week3/server.js
+++ week3/server.js
@@ -2,19 +2,15 @@ const express = require("express")
 const app = express()
 
 
-// Middleware (for request)
 app.use(express.json())
 
 
-//Fake Data
 
   
 
 
-//Routes
 app.use("/bounties", require("./routes/bountyRouter.js"))
 
-//server Listen
 
 app.listen(3000, () => {
     console.log("The server is listening on Port 3000!")
